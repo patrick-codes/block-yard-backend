fc3a22a305bfdf288b82df2f954bb689fcd9b804
patrick-codes <khofilyndon2018@gmail.com>
2025-08-15 12:43:22 +0000

    Added auth controllers with logic

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("./src/config/db");
+const connectDB = require("./confiq/db");
+const userRoute = require("./routes/userRoute");
+const errorMiddleware = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -9,15 +11,15 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 // Routes
-app.use("/api/users", require("./src/routes/userRoutes"));
-app.use("/api/products", require("./src/routes/productRoutes"));
-app.use("/api/orders", require("./src/routes/orderRoutes"));
-app.use("/api/inventory", require("./src/routes/inventoryRoutes"));
+app.use("/api/users", userRoute);
+// app.use("/api/products", require("./routes/productRoute"));
+// app.use("/api/orders", require("./routes/orderRoutes"));
+// app.use("/api/inventory", require("./routes/inventoryRoutes"));
 
 // Error handling middleware
-app.use(require("./src/middleware/errorMiddleware"));
+// app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
