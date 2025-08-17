1434b33a000679570b63223b9b26c3cb1004e1a3
patrick-codes <khofilyndon2018@gmail.com>
2025-08-17 13:42:26 +0000

    minor fixes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -155,7 +155,7 @@ exports.adminMarksOrder = async (req, res) => {
 
     await sendSmsHelper({
       to: phone,
-      content: `Hello ${newuser.name}, your order #${neworder.id} status has been updated to: ${neworder.status}.`,
+      content: `Hello ${newuser.name}, your order #${neworder.id} status has been ${neworder.status}.`,
     });
 
     await sendOrderNotification(neworder, "Shipped");
